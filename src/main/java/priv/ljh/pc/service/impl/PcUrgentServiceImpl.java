b25b54ab464bdc7fa70a613ffe7b0a24f422fb5a
Dorian15 <lijinghailjh@163.com>
2021-03-13 23:59:56 +0800

    2021.3.13 PC端物品详细信息部分

--- src/main/java/priv/ljh/pc/service/impl/PcUrgentServiceImpl.java
+++ src/main/java/priv/ljh/pc/service/impl/PcUrgentServiceImpl.java
@@ -11,6 +11,7 @@ import priv.ljh.utils.MyPage;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -43,6 +44,30 @@ public class PcUrgentServiceImpl extends ServiceImpl<PcUrgentMapper, PcUrgent> i
         }
 
         page = new MyPage(pcUrgentsList.subList(beginIndex, endIndex), total);
+
+        return page;
+    }
+
+    @Override
+    public MyPage searchPcUrgentId(int pageNo, int limit, String idSorted, List<Map> PcUrgent) {
+        MyPage page = null;
+        List<Map> PcUrgentList = new ArrayList<>();
+        PcUrgentList.addAll(PcUrgent);
+        if(idSorted != null && idSorted.startsWith("-")){
+            Collections.reverse(PcUrgentList);
+        }
+        int total = PcUrgentList.size();
+        int maxPageNo = PcUrgentList.size()%limit == 0? PcUrgentList.size()/limit:PcUrgentList.size()/limit + 1;
+        if(pageNo>maxPageNo){
+            pageNo = maxPageNo;
+        }
+        int beginIndex = (pageNo-1)*limit;
+        int endIndex = pageNo*limit;
+        if(endIndex>total){
+            endIndex = total;
+        }
+
+        page = new MyPage(PcUrgentList.subList(beginIndex, endIndex), total);
 
         return page;
     }
